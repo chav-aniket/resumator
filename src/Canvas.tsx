687b69fe8bf6feb0d4411fda6e870069b0f086f0
Aniket Chavan <aniket.chavan360@gmail.com>
2022-04-20 01:04:32 +1000

    Project basis set

---
+++ src/Canvas.tsx
@@ -0,0 +1,11 @@
+import { Center, Box } from "@hope-ui/solid"
+
+const Canvas = () => {
+  return (
+    <Center>
+      <Box bg="violet" w="50%" p="$4" color="white" borderRadius="$2xl"><Center>hello</Center></Box>
+    </Center>
+  )
+}
+
+export default Canvas
